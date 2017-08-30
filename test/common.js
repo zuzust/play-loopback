3d5cdf64948d244b26ac77ca0550ad9568e49a8f
Carles Muiños <carles.ml.dev@gmail.com>
2017-08-30 23:14:01 +0200

    configure ACL's to protect endpoints

--- test/common.js
+++ test/common.js
@@ -2,9 +2,13 @@
 
 const app = require('../server/server')
 const chai = require('chai')
+const supertest = require('supertest')
+
 const expect = chai.expect
+const request = supertest(app)
 
 module.exports = {
   app,
-  expect
+  expect,
+  request,
 }
